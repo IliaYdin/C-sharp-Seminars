32ae62bfc0253bb0b100248502395f722d15bd0a
Ilia Ydin <ilia.iud@mail.ru>
2023-01-26 14:18:33 +0300

    Task004 done переделал

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -3,10 +3,6 @@
 // 2. и выдаёт максимальное из этих чисел.
 // 3. Вывод результата.
 
-// Пример:
-// 2, 3, 7 -> 7
-// 44, 5, 78 -> 78
-// 22, 3, 9 -> 22
 
 Console.Write("Введите число1: ");
 int nomber1 = Convert.ToInt32(Console.ReadLine());
@@ -18,7 +14,9 @@ Console.Write("Введите число3: ");
 int nomber3 = Convert.ToInt32(Console.ReadLine());
 
 int max = nomber1;
-if(nomber2 > max) max = nomber2;
-if(nomber3 > max) max = nomber3;
 
-Console.WriteLine($"Наибольшее число: {max}");
+if (nomber2 > max) max = nomber2;
+if (nomber3 > max) max = nomber3;
+
+Console.Write($"Наибольшее число: {max}");
+
